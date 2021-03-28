7024446256797b5681e1f2931a89c981d8341713
na2387 <21016530+na2387@users.noreply.github.com>
2021-03-28 19:08:35 -0400

    first version

---
+++ javascript/index.js
@@ -0,0 +1,23 @@
+import anime from 'animejs/lib/anime.es';
+
+var animeJsScript = document.createElement('script');
+animeJsScript.setAttribute('src', 'https://cdn.jsdelivr.net/npm/animejs@3.0.1/lib/anime.min.js');
+
+
+// Initialize and add the map
+function initMap() {
+    // The location of Portico
+    const portico = { lat: 40.05071202008899, lng: -75.16789842432206 };
+    const philly = { lat: 39.9999999999, lng: -75.16235161616885 };
+    // The map, centered at Portico
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 11,
+        center: philly,
+    });
+    // The marker, positioned at Portico
+    const marker = new google.maps.Marker({
+        position: portico,
+        map: map,
+    });
+}
+
